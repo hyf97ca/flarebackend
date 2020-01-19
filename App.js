512e74936adc010a615a0a82c240baf5fefb906a
Yi Fei Huang <hyf97ca@gmail.com>
2020-01-19 02:43:07 -0500

    add schedule endpoint

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const authToken = credentials.token;
 
 const origin = "+16473701802"
 
-
+//var date = new Date(2020, 1, 18, 20, 20)
 //console.log(accountSid, authToken)
 
 const client = require('twilio')(accountSid, authToken);
@@ -42,6 +42,9 @@ app.post('/api/text', function(req, res) {
 
     if (typeof numberIn === 'undefined' || isNaN(numberIn))
     {
+        res.json({
+            success: false
+            });
         return;
     }
 
@@ -57,6 +60,44 @@ app.post('/api/text', function(req, res) {
         to: '+1'+ numberIn
     })
     .then(message => console.log(message.sid));
+
+    res.json({
+        success: true
+        });
+    
+});
+
+app.get('/api/schedule', function(req, res) {
+    res.json({
+        "Monday":[
+         {title:"walk the dog", location:"nowhere", start: new Date(2020, 1, 13, 19, 20), end: new Date(2020, 1, 13, 20, 20)}, 
+         {title:"grocery shopping", location:"walmart", start: new Date(2020, 1, 13, 20, 20), end: new Date(2020, 1, 13, 21, 20)}
+        ],
+        "Tuesday":[
+          {title:"walk the dog", location:"nowhere", start: new Date(2020, 1, 13, 19, 20), end: new Date(2020, 1, 13, 20, 20)}, 
+          {title:"swimming", location:"pool", start: new Date(2020, 1, 13, 18, 20), end: new Date(2020, 1, 13, 19, 20)}
+        ],
+        "Wednesday":[
+          {title:"walk the dog", location:"nowhere", start: new Date(2020, 1, 14, 19, 20), end: new Date(2020, 1, 14, 20, 20)}, 
+          {title:"visit children", location:"mexico", start: new Date(2020, 1, 14, 17, 20), end: new Date(2020, 1, 14, 21, 20)}
+        ],
+        "Thursday":[
+          {title:"walk the dog", location:"nowhere", start: new Date(2020, 1, 15, 19, 20), end: new Date(2020, 1, 15, 20, 20)}, 
+          {title:"water plants", location:"home", start: new Date(2020, 1, 15, 19, 00), end: new Date(2020, 1, 15, 19, 20)}
+        ],
+        "Friday":[
+          {title:"walk the dog", location:"nowhere", start: new Date(2020, 1, 16, 19, 20), end: new Date(2020, 1, 16, 20, 20)}, 
+          {title:"regular checkup", location:"doctor", start: new Date(2020, 1, 16, 20, 20), end: new Date(2020, 1, 16, 21, 20)}
+        ],
+        "Saturday":[
+          {title:"walk the dog", location:"nowhere", start: new Date(2020, 1, 17, 19, 20), end: new Date(2020, 1, 17, 20, 20)}, 
+          {title:"go for icecream", location:"walmart", start: new Date(2020, 1, 17, 9, 20), end: new Date(2020, 1, 17, 10, 30)}
+        ],
+        "Sunday":[
+          {title:"walk the dog", location:"nowhere", start: new Date(2020, 1, 18, 19, 20), end: new Date(2020, 1, 18, 20, 20)}, 
+          {title:"bingo", location:"community centre", start: new Date(2020, 1, 18, 20, 20), end: new Date(2020, 1, 18, 21, 20)}
+        ]
+      })
 });
 
 //app listens on port 3000
